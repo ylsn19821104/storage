26cccccd34a9b130d1adde6f548b2f804481164a
ylsn19821104 <ylsn1982@126.com>
2016-05-12 21:31:03 +0800

    库存jsp、js

--- src/main/webapp/WEB-INF/pages/purchaseManage.jsp
+++ src/main/webapp/WEB-INF/pages/purchaseManage.jsp
@@ -7,7 +7,13 @@
     <title>采购单列表</title>
     <jsp:include page="common.jsp"/>
 
-    <script type="text/javascript" src="${pageContext.request.contextPath}/js/purchase.js"></script>
+    <script type="text/javascript" src="${pageContext.request.contextPath}/resources/js/purchase.js"></script>
+    <script type="text/javascript" src="resources/js/databox-formatter.js"></script>
+    <script type="text/javascript">
+    function setDefaultStat(data){
+    	$('#stat').combobox('setValue',0);
+    }
+    </script>
 </head>
 <body>
 <table id="dg" class="easyui-datagrid" title="采购单" fit="true"
@@ -17,13 +23,15 @@
         <th field="id" width="50" align="center" hidden="true">ID</th>
         <th field="cb" checkbox="true" align="center"></th>
         <th data-options="field:'billNo',width:80">采购单号</th>
-        <th data-options="field:'statName',width:80">单据状态</th>
+        <th data-options="field:'stat',width:80,formatter:function(value,row){ return $.statusDic[String(value)];}">单据状态
+        </th>
         <th data-options="field:'billDate',width:80,formatter:
         function(value,row){ if(value) value = new Date(value);
         return value?value.getFullYear()+'-'+value.getMonth()+'-'+value.getDate():'';}">单据日期
         </th>
         <th data-options="field:'supplierName',width:80">供应商</th>
         <th data-options="field:'warehouseName',width:80">入库仓库</th>
+        <th field="warehouseId" hidden="true">warehouseId</th>
         <th data-options="field:'total',width:80">采购总金额</th>
     </tr>
     </thead>
@@ -33,6 +41,8 @@
     <a id="btn_add" href="#" class="easyui-linkbutton" iconCls="icon-add" plain="true">新增</a>
     <a id="btn_edit" href="#" class="easyui-linkbutton" iconCls="icon-edit" plain="true">编辑</a>
     <a id="btn_remove" href="#" class="easyui-linkbutton" iconCls="icon-remove" plain="true">删除</a>
+    <a id="btn_finish" href="#" class="easyui-linkbutton" iconCls="icon-ok" plain="true">审核</a>
+    <a id="btn_unfinish" href="#" class="easyui-linkbutton" iconCls="icon-ok" plain="true">取消审核</a>
 </div>
 <%--以下为编辑面板的内容--%>
 <div id="editPanel" class="easyui-dialog" title="编辑" style="padding:10px;width:700px;height:450px;"
@@ -42,25 +52,27 @@
         <table cellspacing="8px">
             <tr>
                 <td>采购单号：</td>
-                <td><input class="easyui-textbox" type="text" name="billNo" style="width:173px"
+                <td><input class="easyui-textbox" readonly type="text" name="billNo" style="width:173px"
                            data-options="required:true,missingMessage:'必填字段'">
                 </td>
 
-                <td>单据日期：</td>
+				 <td>单据状态：</td>
+                <td>
+                    <select class="easyui-combobox" readonly name="stat" id="stat" style="width:173px;" data-options="
+                    valueField: 'valueField',
+                    textField: 'textField',
+                    
+                    url: '${pageContext.request.contextPath}/dic/billStatus',required:true,editable:false,missingMessage:'必填字段'">
+                    </select>
+                </td>
+                
+                
+            </tr>
+            <tr>
+               	<td>单据日期：</td>
                 <td>
                     <input id="billDate" class="easyui-datebox" name="billDate"
                            style="width:173px" data-options="editable:false">
-                </td>
-            </tr>
-            <tr>
-                <td>单据状态：</td>
-                <td>
-                    <select class="easyui-combobox" name="stat" style="width:173px;" data-options="
-                    valueField: 'id',
-                    textField: 'text',
-                    
-                    url: '${pageContext.request.contextPath}/billStat/comboList',required:true,editable:false,missingMessage:'必填字段'">
-                    </select>
                 </td>
 
                 <td>入库仓库：</td>
@@ -74,11 +86,7 @@
             </tr>
 
             <tr>
-                <td>入库数量：</td>
-                <td><input class="easyui-numberbox" type="text" name="total" id="total" style="width:173px"
-                           data-options="min:1,required:true"></td>
-
-                <td>供应商：</td>
+            	<td>供应商：</td>
                 <td>
                     <select class="easyui-combobox" name="supplierId" style="width:173px;" data-options="
                     valueField: 'id',
@@ -87,6 +95,13 @@
                     </select>
 
                 </td>
+                
+                <td>采购总金额：</td>
+                <td>
+                	<input class="input" readonly type="text" name="total" id="total" style="width:173px">
+                </td>
+
+                
             </tr>
         </table>
 
--- src/main/webapp/WEB-INF/resources/js/purchase.js
+++ src/main/webapp/WEB-INF/resources/js/purchase.js
@@ -1,77 +1,156 @@
-/**
- * Created by luopotaotao on 2016/4/18.
- */
 $(function () {
     bindHandlers();
     bindT2Handlers();
     var t1Url = '/storage/purchase';
     var t2Url = '/storage/purchaseDtl'
-    var currentItem = null;
 
     function bindHandlers() {
-        $('#btn_add').bind('click', add);
-        $('#btn_edit').bind('click', edit);
+        loadDic();
+        $('#btn_add').bind('click', toAdd);
+        $('#btn_edit').bind('click', toEdit);
         $('#btn_query').bind('click', query);
         $('#btn_remove').bind('click', remove);
         $('#btn_edit_save').bind('click', save);
         $('#btn_edit_close').bind('click', closeEditPanel);
+        $('#btn_finish').bind('click', finish);
+        $('#btn_unfinish').bind('click',unfinish);
+    }
+
+    function finish(){
+        $('#btn_finish').unbind();
+        setTimeout(function () {
+            $('#btn_finish').bind(finish);
+        },1000);
+        var rows = $('#dg').datagrid('getChecked');
+        var ids = [];
+        var warehouseIds=[];
+        if (rows && rows.length > 0) {
+            $(rows).each(function (i, v, r) {
+                if(v['stat']!=1){
+                    ids.push(v['id']);
+                    var warehouseId= v['warehouseId'];
+                    warehouseIds.push(warehouseId);
+                }
+            });
+        }
+        if(ids&&ids.length>0){
+            $.ajax({
+                url:t1Url+'/finish',
+                type:'POST',
+                dataType:'json',
+                data:{ids:ids,warehouseIds:warehouseIds}
+            }).success(function (ret) {
+                if(ret&&ret.flag){
+                    $.messager.alert('系统提示','审核完成!');
+                    query();
+                }else{
+                    $.messager.alert('系统提示','操作失败,请重新尝试或联系管理员!');
+                }
+            }).error(function (e) {
+                $.messager.alert('系统提示','操作失败,请重新尝试或联系管理员!');
+            });
+        }else{
+            $.messager.alert('系统提示','请选择未完成的条目进行审核!');
+        }
+    }
+    
+    function unfinish(){
+        $('#btn_unfinish').unbind();
+        setTimeout(function () {
+            $('#btn_unfinish').bind(unfinish);
+        },1000);
+        var rows = $('#dg').datagrid('getChecked');
+        var ids = [];
+        var warehouseIds=[];
+        if (rows && rows.length > 0) {
+            $(rows).each(function (i, v, r) {
+                if(v['stat']!=0){
+                    ids.push(v['id']);
+                    warehouseIds.push(v['warehouseId']);
+                }
+            });
+        }
+        if(ids&&ids.length>0){
+            $.ajax({
+                url:t1Url+'/unfinish',
+                type:'POST',
+                dataType:'json',
+                data:{ids:ids,warehouseIds:warehouseIds}
+            }).success(function (ret) {
+                if(ret&&ret.flag){
+                    $.messager.alert('系统提示','取消审核完成!');
+                    query();
+                }else{
+                    $.messager.alert('系统提示','操作失败,请重新尝试或联系管理员!');
+                }
+            }).error(function (e) {
+                $.messager.alert('系统提示','操作失败,请重新尝试或联系管理员!');
+            });
+        }else{
+            $.messager.alert('系统提示','请选择未完成的条目进行审核!');
+        }
+    }
+    
+    function loadDic() {
+        $.ajax({
+            url: '/storage/dic/billStatus',
+            type: 'get'
+        }).success(function (ret) {
+            if (ret && ret.length) {
+                var dic = {};
+                $(ret).each(function (i, val) {
+                    dic[val.valueField] = val.textField;
+                })
+                $.statusDic = dic;
+            } else {
+                $.messager.alert('系统提示', '加载数据字典失败,请刷新页面或联系管理员!');
+            }
+        }).error(function () {
+            $.messager.alert('系统提示', '加载数据字典失败,请刷新页面或联系管理员!');
+        });
     }
 
     function bindT2Handlers() {
-        $('#btn_t2_add').bind('click', t2Add);
-        $('#btn_t2_edit').bind('click', t2Edit);
+        $('#btn_t2_add').bind('click', t2ToAdd);
+        $('#btn_t2_edit').bind('click', t2ToEdit);
         $('#btn_t2_query').bind('click', t2Query);
         $('#btn_t2_remove').bind('click', t2Remove);
         $('#btn_t2_edit_save').bind('click', t2Save);
         $('#btn_t2_edit_close').bind('click', t2CloseEditPanel);
     }
 
-    function add() {
-        currentItem = null;
-        switchAddOrEdit(true);
+    function toAdd() {
+        $.currentItem = {type:'new'};
         $('#editForm').form('clear');
+        $('#stat').combobox('setValue',0);
+        $("#t2_dg").datagrid('loadData',{total:0,rows:[]});
         $('#editPanel').dialog('open');
     }
 
-    function edit() {
+    function formatDate(obj,names){
+        function format(name) {
+            obj[name] = (obj&&obj[name])?new Date(obj[name]).format("yyyy-MM-dd"):null;
+        }
+        for(var i in names){
+            obj[names[i]] = format(names[i]);
+        }
+    }
+    function toEdit() {
         var rows = $('#dg').datagrid('getChecked');
         if (rows) {
             if (rows.length > 1) {
                 $.messager.alert('系统提示!', '只能对一行进行编辑!')
             } else if (rows.length == 1) {
-                currentItem = rows[0];
-                var url = t1Url + '/findById.do';
-                $.ajax({
-                    url: url,
-                    data: {id: rows[0]['id']},
-                    dataType: 'json',
-                    type: 'get'
-                }).success(function (ret) {
-                    if (ret && ret['id']) {
-                        $('#t2_dg').datagrid('loadData', {total: 0, rows: []});
-                    }
-                    t2Query();
-                    if(ret.beginDate){
-                        var value = new Date(ret.beginDate);
-                        //ret.beginDate = (value.getMonth()+1)+'/'+value.getDate()+'/'+value.getFullYear();
-                        ret.beginDate = value.getFullYear()+'-'+(value.getMonth()+1)+'-'+value.getDate();
-                    }
-                    if(ret.endDate){
-                        var value = new Date(ret.endDate);
-                        //ret.endDate = (value.getMonth()+1)+'/'+value.getDate()+'/'+value.getFullYear();
-                        ret.endDate = value.getFullYear()+'-'+(value.getMonth()+1)+'-'+value.getDate();
-                    }
-                    //ret.c12 = null;
-                    //ret.c13 = null;
-                    $('#editForm').form('load', ret);
-                    $(ret).each(function (i, val) {
-                        $('#' + i).val(val);
-                    });
-                    $('#editPanel').dialog('open');
-                    switchAddOrEdit(false);
-                }).error(function (err) {
-                    $.messager.alert('系统提示!', '获取数据!请重新尝试或联系管理员!');
-                });
+                var row = rows[0];
+
+                formatDate(row,['billDate','create_time','update_time']);
+
+                $.currentItem = row;
+
+                $('#editForm').form('load', $.currentItem);
+                t2Query();
+                $('#editPanel').dialog('open');
+
             }
         }
 
@@ -90,7 +169,7 @@ $(function () {
         }
         if (ids.length > 0) {
             $.ajax({
-                url: t1Url + '/delete.do',
+                url: t1Url + '/delete',
                 data: {ids: ids},
                 dataType: 'json',
                 type: 'post'
@@ -108,7 +187,7 @@ $(function () {
     }
 
     function query() {
-    	var url = t1Url + '/list.do';
+        var url = t1Url + '/list';
         $.ajax({
             url: url,
             dataType: 'json',
@@ -126,126 +205,138 @@ $(function () {
 
     function save() {
         if ($('#editForm').form('validate')) {
-            $('#editForm').form('submit', {
-                    url: t1Url + (currentItem ? '/update.do' : '/save.do'),
-                    ajax: false,
-                    contentType: "application/json",
-                    onSubmit: function () {
-                        $.messager.progress();
-                        return true;	// 返回false终止表单提交
-                    },
-                    success: function (ret) {
-                        $.messager.progress('close');	// 如果提交成功则隐藏进度条
-                        $.messager.alert('系统提示!', '保存成功!');
+            $('#editForm input').each(function (i, val) {
+                var name = $(val).attr('name');
+                if (name&&$(val).val()) {
+                    $.currentItem[name] = $(val).val();
+                }
+            });
+            var details = $('#t2_dg').datagrid('getData').rows;
+            $(details).each(function (i,val) {
+                if(val.dtlId<0){
+                    delete val.dtlId;
+                }
+            })
+            if (details)
+                $.currentItem.details = JSON.stringify(details);
+            delete $.currentItem.type;
+            $.ajax({
+                url: t1Url + '/save',
+                type: 'post',
+                data: $.currentItem
+            }).success(function (ret) {
+                if (ret && ret.flag) {
+                    $.messager.alert('系统提示!', '保存成功!');
+                    //如果是修改的话不关闭当前页面,新增的话才关闭
+                    if($.currentItem.id){
+                        $.get(t1Url+'/findById',{id:$.currentItem.id},function (data) {
+                            formatDate(data,['billDate','create_time','update_time']);
+                            $.currentItem = data;
+                            $('#editForm').form('load', data);
+                        });
+                        query();
+                        t2Query();
+                    }else{
                         $('#editForm').form('clear');
                         $('#editPanel').dialog('close');
-                        query();
-                    },
-                    error: function (err) {
-                        $.messager.progress('close');
-                        $.messager.alert('系统提示!', '保存失败,请重新尝试或联系管理员!');
                     }
+
+                    query();
+                } else {
+                    $.messager.alert('系统提示!', '保存失败,请重新尝试或联系管理员!');
                 }
-            );
+            }).error(function (e) {
+                $.messager.alert('系统提示!', '保存失败,请重新尝试或联系管理员!');
+            }).complete(function (e) {
+                $.currentItem = {};
+                $.messager.progress('close');
+            });
         }
 
 
     }
 
-    function t2Add() {
-        var id = currentItem['id'];
+    function t2ToAdd() {
+        $.t2CurrentItem = {dtlId:-(new Date().getTime()), type: 'new'};
         $('#t2EditPanel').dialog('open');
         $('#t2EditForm').form('clear');
-        $('#t2EditForm').form('load', {id: id});
+        $('#t2EditForm').form('load', {dtlId: $.t2CurrentItem});
     }
 
-    function t2Edit() {
+    function t2ToEdit() {
         var rows = $('#t2_dg').datagrid('getChecked');
         if (rows) {
             if (rows.length > 1) {
                 $.messager.alert('系统提示!', '只能对一行进行编辑!')
             } else if (rows.length == 1) {
-            	var url=t2Url + '/findById.do';
-                $.ajax({
-                    url: url,
-                    data: {id: rows[0]['dtlId']},
-                    dataType: 'json',
-                    type: 'get'
-                }).success(function (ret) {
-                    if (ret && ret['dtlId']) {
-                        $('#t2EditForm').form('clear');
-                        $('#t2EditForm').form('load', ret);
-                        $('#t2EditPanel').dialog('open');
-                        switchAddOrEdit(false);
-                    } else {
-                        $.messager.alert('系统提示!', '获取数据失败!请重新尝试或联系管理员!');
-                    }
-                }).error(function (err) {
-                    $.messager.alert('系统提示!', '获取数据!请重新尝试或联系管理员!');
-                });
+                $('#t2EditForm').form('clear');
+                $.t2CurrentItem = rows[0];
+                $('#t2EditForm').form('load', $.t2CurrentItem);
+                $('#t2EditPanel').dialog('open');
             }
         } else {
             $.messager.alert('系统提示!', '请选择要编辑的行!')
         }
     }
-
+    
+    function calcTotal(){
+    	var rows = $('#t2_dg').datagrid('getRows');
+    	var total=null;
+    	
+    	if(rows && rows.length){
+    		total=0;
+    		$.each(rows,function(i,row){
+    			var itemTotal = parseFloat(row['itemTotal']);
+    			total+=(isNaN(itemTotal)? 0 :itemTotal);
+    		});
+    	}
+    	
+    	$('#total').val(total);
+    }
+    
     function t2Save() {
         if ($('#t2EditForm').form('validate')) {
-        	var url =t2Url + ($('#dtlId').val() ? '/update.do' : '/save.do')+'?id='+currentItem['id']; 
-            $('#t2EditForm').form('submit', {
-                url: url,
-                ajax: false,
-                contentType: "application/json",
-                onSubmit: function () {
-                    $.messager.progress();
-                    return true;	// 返回false终止表单提交
-                },
-                success: function (ret) {
-                    $.messager.progress('close');	// 如果提交成功则隐藏进度条
-                    $.messager.alert('系统提示!', '保存成功!');
-                    $('#t2EditForm').form('clear');
-                    $('#t2EditPanel').dialog('close');
-                    t2Query();
-                },
-                error: function (err) {
-                    $.messager.progress('close');
-                    $.messager.alert('系统提示!', '保存失败,请重新尝试或联系管理员!');
+            var item = $.t2CurrentItem;
+            if (item) {
+                var type = item.type
+                $('#t2EditForm input').each(function (i, val) {
+                    var key = $(val).attr('name');
+                    if (key) {
+                        item[key] = $(val).val();
+                    }
+                });
+                if (item.type) {
+                    delete item.type;
+                    $('#t2_dg').datagrid('appendRow', item);
+                } else {
+                    var index = $('#t2_dg').datagrid('getRowIndex', item);
+                    $('#t2_dg').datagrid('updateRow', {index: index, row: item});
                 }
             }
-            );
+            
+            calcTotal();
+            delete $.t2CurrentItem;
+            $('#t2EditForm').form('clear');
+            $('#t2EditPanel').dialog('close');
         }
     }
 
     function t2Remove() {
-        var rows = $('#t2_dg').datagrid('getChecked');
-        var ids = [];
+        var grid = $('#t2_dg');
+        var rows = grid.datagrid('getChecked');
+
         if (rows && rows.length > 0) {
             $(rows).each(function (i, v, r) {
-                ids.push(v['dtlId']);
+                var index = grid.datagrid('getRowIndex', v);
+                grid.datagrid('deleteRow', index);
             });
         }
-        if (ids.length > 0) {
-        	var url = t2Url + '/delete.do';
-            $.ajax({
-                url: url,
-                data: {ids: ids},
-                dataType: 'json',
-                type: 'post'
-            }).success(function (ret) {
-                if (ret && ret.flag > 0) {
-                    $.messager.alert('系统提示!', '删除成功!');
-                    t2Query();
-                } else {
-                    $.messager.alert('系统提示!', '删除失败!请重新尝试或联系管理员!');
-                }
-            }).error(function (err) {
-                $.messager.alert('系统提示!', '删除失败!请重新尝试或联系管理员!');
-            });
-        }
+        
+        calcTotal();
     }
 
     function t2Query() {
-    	var url = t2Url + '/findAllById.do?id=' + currentItem['id'];
+        var url = t2Url + '/findAllById?id=' + $.currentItem['id'];
         $.ajax({
             url: url,
             dataType: 'json',
@@ -266,18 +357,8 @@ $(function () {
     }
 
     function t2CloseEditPanel() {
-        currentItem = null;
+        $.currentItem = null;
         $('#t2EditPanel').dialog('close');
-    }
-
-    function switchAddOrEdit(flag) {
-        if (flag) {
-            $('#t2_panel').hide();
-            //$('#c01').numberbox({editable: true});
-        } else {
-            $('#t2_panel').show();
-            //$('#c01').numberbox({editable: false});
-        }
     }
 })
 ;
